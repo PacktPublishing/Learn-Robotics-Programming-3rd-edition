70525745dcc69e6779bf977395a52d36adf8e619
Danny Staple <danny@orionrobots.co.uk>
2023-12-23 16:06:27 +0000

    make the mqtt_behavior bit common

---
+++ robot/mqtt_behavior.py
@@ -0,0 +1,20 @@
+import paho.mqtt.client as mqtt
+import time
+
+def on_connect(client, userdata, flags, rc):
+    print(f"Connected with result code {rc}")
+
+def connect():
+    mqtt_username = "robot"
+    mqtt_password = "robot"
+
+    client = mqtt.Client()
+    client.username_pw_set(mqtt_username, mqtt_password)
+    client.on_connect = on_connect
+    client.will_set("motors/stop", "")
+    client.connect("localhost", 1883)
+
+    client.loop_start()
+    while not client.is_connected():
+        time.sleep(0.01)
+    return client
