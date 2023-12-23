fc83c259adb8a97bb17448fbc1bfb155d935b4a5
Danny Staple <danny@orionrobots.co.uk>
2023-12-23 16:35:02 +0000

    Add the path behavior

--- robot/behavior_line.py
+++ robot/behavior_line.py
@@ -1,9 +1,9 @@
-import time
-from robot.mqtt_behavior import connect
+from time import sleep
+from mqtt_behavior import connect
 
 client = connect()
 client.publish("motors/left", 0.8)
 client.publish("motors/right", 0.8)
-time.sleep(2)
+sleep(2)
 client.publish("motors/stop", "")
 client.loop_stop()
