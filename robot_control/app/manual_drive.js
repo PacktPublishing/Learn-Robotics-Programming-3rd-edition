2539d7dbef3cbcfbb91650ee3e6aee582c4ffcaa
Danny Staple <danny@orionrobots.co.uk>
2024-01-01 11:52:34 +0000

    Add joystick

--- robot_control/app/manual_drive.js
+++ robot_control/app/manual_drive.js
@@ -1,13 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { Text } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { KorolJoystick } from "korol-joystick";
 import { styles } from '../styles';
 
 export default function Page() {
     return (
-      <View style={styles.container}>
+      <GestureHandlerRootView style={styles.container}>
         <Text style={styles.title}>Manual drive</Text>
         <Link href="/">Back</Link>
+        <KorolJoystick color="#06b6d4" radius={75} onMove={onJoystickMove} onStop={onJoystickStop }/>
         <StatusBar style="auto" />
-      </View>
+      </GestureHandlerRootView>
     );
   }
+
+function onJoystickMove(data) {
+  console.log('Joystick move');
+  console.log(data);
+}
+
+function onJoystickStop() {
+  console.log('Joystick stop');
+}
