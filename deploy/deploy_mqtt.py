660aad93dacffd97afa19cf6274320efbe6c8cbd
Danny Staple <danny@orionrobots.co.uk>
2024-01-01 16:01:44 +0000

    Enabling Websockets in mosquitto

--- deploy/deploy_mqtt.py
+++ deploy/deploy_mqtt.py
@@ -1,5 +1,5 @@
 from pyinfra.operations import \
-    apt, systemd, server
+    apt, systemd, server, files
 
 
 mosquitto_packages = apt.packages(
@@ -19,7 +19,14 @@ if mosquitto_packages.changed:
         f"mosquitto_passwd -c -b /etc/mosquitto/passwd {mqtt_username} {mqtt_password}",
         _sudo=True)
 
-if mosquitto_packages.changed:
+mosquitto_files = files.put(
+    name="Configure mosquitto",
+    src="deploy/robot_mosquitto.conf",
+    dest="/etc/mosquitto/conf.d/robot.conf",
+    _sudo=True
+)
+
+if mosquitto_packages.changed or mosquitto_files.changed:
     # restart mosquitto
     systemd.service(
         name="Restart/enable mosquitto",
