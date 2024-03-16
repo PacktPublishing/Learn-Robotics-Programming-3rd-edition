9562657ed692a3a4bc30edaa7117e9ad891eee95
Danny Staple <danny@orionrobots.co.uk>
2024-03-16 17:06:53 +0000

    Encdoer plotting

--- robot_control/app/behavior_line.js
+++ robot_control/app/behavior_line.js
@@ -1,23 +1,27 @@
 import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { View , Text } from 'react-native';
-import { Link, useNavigation } from 'expo-router';
+import { useNavigation } from 'expo-router';
 import { styles, BackButton } from '../lib/styles';
+import { env } from '../lib/connection'
+import { WebView } from 'react-native-webview';
 
-export default function Page({ navigation }) {
+export default function Page() {
     const [encoderData, setEncoderData] = useState({left_radians: 0.0, right_radians: 0.0});
-    global.mqttClient.subscribe("sensors/encoders/data");
-    global.mqttClient.onMessageArrived = (message) => {
-        const logData = JSON.parse(message.payloadString);
-        setEncoderData(logData);
-    }
 
     useNavigation().addListener('beforeRemove', (e) => {
         global.mqttClient.unsubscribe("sensors/encoders/data");
     });
 
-
-    global.mqttClient.publish("launcher/start", "behavior_line.service");
+    useNavigation().addListener('focus', () => {
+        global.mqttClient.publish("launcher/start", "behavior_line.service");
+        global.mqttClient.subscribe("sensors/encoders/data");
+        global.mqttClient.onMessageArrived = (message) => {
+            const logData = JSON.parse(message.payloadString);
+            setEncoderData(logData);
+        }
+    
+    });
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Drive line</Text>
@@ -25,6 +29,9 @@ export default function Page({ navigation }) {
             <StatusBar style="auto" />
             <Text>Left Encoder: {encoderData.left_radians}</Text>
             <Text>Right Encoder: {encoderData.right_radians}</Text>
+            <WebView style={styles.container}
+                source={{uri: "http://" + env.PI_HOSTNAME + ":5001/"}}
+                />
         </View>
     );
 }
