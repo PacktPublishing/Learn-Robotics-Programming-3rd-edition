c29fe12c6cff4dabcbb7df21d8e28bf938f0368b
Danny Staple <danny@orionrobots.co.uk>
2024-10-07 20:37:45 +0100

    Updated deploy style

--- deploy/deploy_services.py
+++ deploy/deploy_services.py
@@ -1,43 +1,41 @@
 from pyinfra.operations import files, systemd
 from pyinfra import host
 
-service_name = "inventor_hat_service"
-python_file = "robot/inventor_hat_service.py"
-auto_start = True
+def deploy_service(service_name, command, auto_start, changed):
+    if auto_start:
+        restart="always"
+    else:
+        restart="no"
+
+    unit_file = files.template(
+        name=f"Create {service_name} service",
+        src="deploy/service_template.j2",
+        dest=f"/etc/systemd/system/{service_name}.service",
+        pi_user=host.data.get('ssh_user'),
+        service_name=service_name,
+        command=command,
+        restart=restart,
+        _sudo=True
+    )
+        
+    if changed or code.changed or unit_file.changed:
+        systemd.service(
+            name=f"Restart {service_name} service",
+            service=service_name,
+            running=auto_start,
+            enabled=auto_start,
+            restarted=auto_start,
+            daemon_reload=unit_file.changed,
+            _sudo=True,
+        )
 
 common = files.sync(
     name="Update common code",
     src="robot/common", dest="robot/common")
 
 code = files.put(
-    name=f"Update {service_name} code",
-    src=python_file,
-    dest=python_file,
-)
+    name="Update inventor hat code",
+    src="robot/inventor_hat_service.py", dest="robot/inventor_hat_service.py")
 
-if auto_start:
-    restart="always"
-else:
-    restart="no"
-
-unit_file = files.template(
-    name=f"Create {service_name} service",
-    src="deploy/service_template.j2",
-    dest=f"/etc/systemd/system/{service_name}.service",
-    pi_user=host.data.get('ssh_user'),
-    service_name=service_name,
-    command=python_file,
-    restart=restart,
-    _sudo=True
-)
-
-if code.changed or common.changed or unit_file.changed:
-    systemd.service(
-        name=f"Restart {service_name} service",
-        service=service_name,
-        running=auto_start,
-        enabled=auto_start,
-        restarted=auto_start,
-        daemon_reload=unit_file.changed,
-        _sudo=True,
-    )
+deploy_service("inventor_hat_service","robot/inventor_hat_service.py", 
+               True, common.changed or code.changed)
