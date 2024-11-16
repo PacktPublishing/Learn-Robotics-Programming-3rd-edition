52fb97f02bc35288ea2bccccd43c09e4ca36c8a2
Danny Staple <danny@orionrobots.co.uk>
2024-11-16 15:38:28 +0000

    Bug fix

--- robot/distance_sensor_service.py
+++ robot/distance_sensor_service.py
@@ -3,7 +3,10 @@ import time
 import vl53l5cx_ctypes
 from common.mqtt_behavior import connect, publish_json
 
-VALID_STATUSES = (vl53l5cx_ctypes.STATUS_RANGE_VALID, vl53l5cx_ctypes.STATUS_RANGE_VALID_LARGE_PULSE)
+VALID_STATUSES = (vl53l5cx_ctypes.STATUS_RANGE_VALID,
+                  vl53l5cx_ctypes.STATUS_RANGE_VALID_LARGE_PULSE)
+
+
 class DistanceSensorService:
     def __init__(self):
         self.sensor = vl53l5cx_ctypes.VL53L5CX()
@@ -25,15 +28,18 @@ class DistanceSensorService:
         # Skip low confidence data
         for n, data in enumerate(list(data.target_status[0])):
             if data not in VALID_STATUSES:
-                as_list[n] = 3000 # max 3m.
+                as_list[n] = 3000  # max 3m.
         publish_json(self.client, "sensors/distance_mm", as_list)
 
     def loop_forever(self):
         print("Making connection")
         self.client = connect()
         self.client.subscribe("sensors/distance/control/#")
-        self.client.message_callback_add("sensors/distance/control/start_ranging", self.start_ranging)
-        self.client.message_callback_add("sensors/distance/control/stop_ranging", self.stop_ranging)
+        self.client.subscribe("all/stop")
+        self.client.message_callback_add(
+            "sensors/distance/control/start_ranging", self.start_ranging)
+        self.client.message_callback_add(
+            "sensors/distance/control/stop_ranging", self.stop_ranging)
         self.client.publish("sensors/distance/status", "ready")
         self.client.message_callback_add("all/stop", self.stop_ranging)
         print("Starting loop")
@@ -41,6 +47,7 @@ class DistanceSensorService:
             if self.is_ranging and self.sensor.data_ready():
                 self.update_data()
             time.sleep(0.01)
+
 
 print("Initialising sensor")
 service = DistanceSensorService()
