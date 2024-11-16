39d6f51585b8d2afc2a44891760ce86363d37f14
Danny Staple <danny@orionrobots.co.uk>
2024-11-16 15:39:06 +0000

    Add missing link

--- robot/launcher_service.py
+++ robot/launcher_service.py
@@ -5,19 +5,23 @@ from common.mqtt_behavior import connect, publish_json
 def print_message(client, userdata, msg):
     print(f"{msg.topic} {msg.payload}")
 
+
 def start_systemd_unit(client, userdata, msg):
     unit_name = msg.payload.decode()
     subprocess.run(["systemctl", "start", unit_name])
     print(unit_name, "started")
+
 
 def stop_systemd_unit(client, userdata, msg):
     unit_name = msg.payload.decode()
     subprocess.run(["systemctl", "stop", unit_name])
     print(unit_name, "stopped")
 
+
 def poweroff(client, userdata, msg):
     print("Powering off")
     subprocess.run(["poweroff"])
+
 
 def on_connect(client, userdata, flags, rc):
     print(f"Connected with result code {rc}")
@@ -29,7 +33,7 @@ client = connect(on_connect=on_connect, start_loop=False)
 
 client.message_callback_add("launcher/#", print_message)
 client.message_callback_add("launcher/start", start_systemd_unit)
-# client.message_callback_add("launcher/stop", stop_systemd_unit)
+client.message_callback_add("launcher/stop", stop_systemd_unit)
 client.message_callback_add("launcher/poweroff", poweroff)
 
 client.loop_forever()
