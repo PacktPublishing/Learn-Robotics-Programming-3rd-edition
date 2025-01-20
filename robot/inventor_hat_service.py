0a51f563d8e91f0d68aebe53c5d566097b0a9414
Danny Staple <danny@orionrobots.co.uk>
2025-01-20 19:54:16 +0000

    Add servo control

--- robot/inventor_hat_service.py
+++ robot/inventor_hat_service.py
@@ -1,6 +1,8 @@
 import atexit
 import json
 import time
+from functools import partial
+
 import inventorhatmini
 import paho.mqtt.client as mqtt
 
@@ -8,12 +10,30 @@ last_message = 0
 board = inventorhatmini.InventorHATMini()
 left_motor = board.motors[1]
 right_motor = board.motors[0]
+pan = board.servos[0]
+tilt = board.servos[1]
 
 
 def all_messages(client, userdata, msg):
     global last_message
     last_message = time.time()
     print(f"{msg.topic} {msg.payload}")
+
+
+def set_servo_position(servo, client, userdata, msg):
+    try:
+        position = int(msg.payload)
+        servo.enable()
+        servo.value(position)
+    except OSError:
+        print("Error: Failed to set servo position")
+    except ValueError:
+        print("Error: Invalid position value")
+
+
+def stop_servo(servo, client=None, userdata=None, msg=None):
+    if servo.is_enabled():
+        servo.disable()
 
 
 def set_motor_wheels(client, userdata, msg):
@@ -27,6 +47,8 @@ def set_motor_wheels(client, userdata, msg):
 def stop_motors(client=None, userdata=None, msg=None):
     left_motor.stop()
     right_motor.stop()
+    stop_servo(pan)
+    stop_servo(tilt)
 
 
 def on_connect(client, userdata, flags, rc):
@@ -35,9 +57,11 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe("leds/#")
     client.subscribe("all/#")
 
+
 def exit_handler():
     stop_motors()
     board.leds.clear()
+
 
 atexit.register(exit_handler)
 
@@ -51,6 +75,14 @@ client.on_connect = on_connect
 client.message_callback_add("motors/#", all_messages)
 client.message_callback_add("motors/stop", stop_motors)
 client.message_callback_add("motors/wheels", set_motor_wheels)
+client.message_callback_add("motors/servo/pan/position",
+                            partial(set_servo_position, pan))
+client.message_callback_add("motors/servo/pan/stop",
+                            partial(stop_servo, pan))
+client.message_callback_add("motors/servo/tilt/position",
+                            partial(set_servo_position, tilt))
+client.message_callback_add("motors/servo/tilt/stop",
+                            partial(stop_servo, tilt))
 client.message_callback_add("all/stop", stop_motors)
 client.message_callback_add("all/#", all_messages)
 
