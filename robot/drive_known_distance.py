b773dc9cefec7b016582b904af38bc1639e26d09
Danny Staple <danny@orionrobots.co.uk>
2025-03-17 22:06:05 +0000

    I was somehow between versions there.

--- robot/drive_known_distance.py
+++ robot/drive_known_distance.py
@@ -3,22 +3,28 @@ import time
 
 from common.mqtt_behavior import connect, publish_json
 from common.pid_controller import PIDController
+from common.time_stepper import TimeStepper
 
 class DriveKnownDistanceBehavior:
     def __init__(self):
-        self.expected_distance = 1000
+        self.time_stepper = TimeStepper()
         self.distance_pid = PIDController(0.2, 0.01)
-        self.last_update = time.time()
+        self.expected_distance = 1000
         self.distance_reached = False
 
-    def calculate_speed(self, client, distance_data, time_difference):
+    def on_encoders_data(self, client, userdata, msg):
+        # Sense
+        distance_data = json.loads(msg.payload)
+        time_difference = self.time_stepper.step()
         left_error = self.expected_distance - distance_data['left_distance']
         right_error = self.expected_distance - distance_data['right_distance']
+
         publish_json(client, "drive_known_distance/plot", {
             "left_error": left_error,
             "right_error": right_error,
             "time": time.time()
         })
+        # Think
         if abs(left_error) > abs(right_error):
             distance_error = left_error
         else:
@@ -27,29 +33,9 @@ class DriveKnownDistanceBehavior:
             self.distance_reached = True
             speed = 0
         else:
-            speed = self.distance_pid.control(distance_error, time_difference),
-        return speed
-
-    def motor_speed_correction(self, speed, distance_data, time_difference):
-        left_mm_error = speed - distance_data['left_mm_per_sec']
-        right_mm_error = speed - distance_data['right_mm_per_sec']
-        left_control = self.left_pid.control(left_mm_error, time_difference)
-        right_control = self.right_pid.control(right_mm_error, time_difference)
-        return left_control, right_control
-
-    def time_step(self):
-        new_time = time.time()
-        time_difference = new_time - self.last_update
-        self.last_update = new_time
-        return time_difference
-
-    def on_encoders_data(self, client, userdata, msg):
-        distance_data = json.loads(msg.payload)
-        time_difference = self.time_step()
-        speed = self.calculate_speed(client, distance_data, time_difference)
-        left_control, right_control = self.motor_speed_correction(
-            speed, distance_data, time_difference)
-        publish_json(client, "motors/wheels", [left_control, right_control])
+            speed = self.distance_pid.control(distance_error, time_difference)
+        # Act
+        publish_json(client, "wheel_control/wheel_speed_mm", [speed, speed])
 
     def on_config_updated(self, client, userdata, message):
         data = json.loads(message.payload)
