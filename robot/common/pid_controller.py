3813d310a480ab41394ec29c5b47883ecfecbc4e
Danny Staple <danny@orionrobots.co.uk>
2025-03-17 20:49:20 +0000

    PID service - ability to hadnle config messages

--- robot/common/pid_controller.py
+++ robot/common/pid_controller.py
@@ -11,3 +11,10 @@ class PIDController:
 
     def reset(self):
         self.integral = 0
+
+    def handle_config_messages(self, data, prefix):
+        if prefix + '/proportional' in data:
+            self.proportional_constant = data[prefix + '/proportional']
+        if prefix + '/integral' in data:
+            self.integral_constant = data[prefix + '/integral']
+            self.integral = 0
