502e0153d20f72dc8c3e0458d3a64fa9e49fd81e
Danny Staple <danny@orionrobots.co.uk>
2025-04-08 20:56:00 +0100

    Publishing the map on MQTT

--- robot/localisation.py
+++ robot/localisation.py
@@ -3,6 +3,15 @@ import numpy as np
 from common.mqtt_behavior import connect, publish_json
 from common.poses import rotated_poses, translated_poses
 
+walls = [
+    (0, 1500),
+    (1500, 1500),
+    (1500, 500),
+    (1000, 500),
+    (1000, 0),
+    (0, 0)
+]
+
 class Localisation:
     def __init__(self):
         self.poses = np.array([(500, 500, 0), (150, 100, np.pi / 3)])
@@ -13,9 +22,15 @@ class Localisation:
         # send_poses = rotate_poses(self.poses)
         publish_json(client, "localisation/poses", self.poses.tolist())
 
+    def publish_map(self, client):
+        publish_json(client, "localisation/map", {
+            "walls": walls
+        })
+
     def start(self):
         client = connect()
         self.publish_poses(client)
+        self.publish_map(client)
 
 service = Localisation()
 service.start()
