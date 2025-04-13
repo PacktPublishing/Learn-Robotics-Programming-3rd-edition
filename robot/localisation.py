fa58e43b49ed2a8143cde887017d054b2153f97b
Danny Staple <danny@orionrobots.co.uk>
2025-04-13 23:14:03 +0100

    We have pose movement

--- robot/localisation.py
+++ robot/localisation.py
@@ -1,3 +1,6 @@
+import ujson as json
+import time
+
 import numpy as np
 
 from common.mqtt_behavior import connect, publish_json
@@ -11,12 +14,25 @@ walls = [
     (1000, 0),
     (0, 0)
 ]
+population_size = 200
 
 class Localisation:
     def __init__(self):
-        self.poses = np.array([(500, 500, 0), (150, 100, np.pi / 3)])
-        self.poses = np.append(self.poses, rotated_poses(self.poses, np.pi/2), 0)
-        self.poses = translated_poses(self.poses, 100)
+        self.poses = np.column_stack((
+            np.random.uniform(0, 1500, population_size),
+            np.random.uniform(0, 1500, population_size),
+            np.random.uniform(0, 2 * np.pi, population_size)
+        ))
+        self.wheel_distance = 0
+        self.config_ready = False
+        self.previous_left_distance = 0
+        self.previous_right_distance = 0
+
+    def on_config_updated(self, client, userdata, message):
+        self.config_ready = True
+        data = json.loads(message.payload)
+        if 'robot/wheel_distance' in data:
+            self.wheel_distance = data['robot/wheel_distance']
 
     def publish_poses(self, client):
         # send_poses = rotate_poses(self.poses)
@@ -27,10 +43,54 @@ class Localisation:
             "walls": walls
         })
 
+    def convert_encoders_to_motion(self, left_distance_delta, right_distance_delta):
+        # Special case, straight line
+        if left_distance_delta == right_distance_delta:
+            return left_distance_delta, 0
+
+        mid_distance = (left_distance_delta + right_distance_delta) / 2
+        theta = (right_distance_delta - left_distance_delta) / self.wheel_distance
+
+        return mid_distance, theta
+
+    def on_encoders_data(self, client, userdata, msg):
+        # Sense
+        distance_data = json.loads(msg.payload)
+        left_distance_delta = distance_data['left_distance'] - self.previous_left_distance
+        right_distance_delta = distance_data['right_distance'] - self.previous_right_distance
+        self.previous_left_distance = distance_data['left_distance']
+        self.previous_right_distance = distance_data['right_distance']
+
+        # Think
+        mid_distance, theta = self.convert_encoders_to_motion(left_distance_delta, right_distance_delta)
+        # Act
+        self.poses = rotated_poses(self.poses, theta / 2)
+        self.poses = translated_poses(self.poses, mid_distance)
+        self.poses = rotated_poses(self.poses, theta / 2)
+        self.publish_poses(client)
+
     def start(self):
         client = connect()
         self.publish_poses(client)
         self.publish_map(client)
+        print("Waiting for config")
+        client.subscribe("config/updated")
+        client.message_callback_add("config/updated",
+                                    self.on_config_updated)
+        publish_json(client, "config/get", [
+                        "robot/wheel_distance",
+                        ])
+        while not self.config_ready:
+            time.sleep(0.1)
+        print("Config received. Now wiating for sensor data...")
+
+        client.subscribe("sensors/encoders/data")
+        client.publish("sensors/encoders/control/reset")
+        client.message_callback_add("sensors/encoders/data",
+                                    self.on_encoders_data)
+        while True:
+            time.sleep(0.1)
+
 
 service = Localisation()
 service.start()
