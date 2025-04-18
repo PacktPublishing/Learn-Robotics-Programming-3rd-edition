82d6c811802e66a12742e51c56050f0df54e960f
Danny Staple <danny@orionrobots.co.uk>
2025-04-18 23:11:55 +0100

    Localisation

--- robot/localisation.py
+++ robot/localisation.py
@@ -6,23 +6,34 @@ import numpy as np
 from common.mqtt_behavior import connect, publish_json
 from common.poses import rotated_poses, translated_poses
 
+width = 1200
+height = 1800
+cutout_left = 900
+cutout_top = 800
+# width = 1500
+# height = 1500
+# cutout_left = 1000
+# cutout_top = 500
+
 walls = [
-    (0, 1500),
-    (1500, 1500),
-    (1500, 500),
-    (1000, 500),
-    (1000, 0),
+    (0, height),
+    (width, height),
+    (width, cutout_top),
+    (cutout_left, cutout_top),
+    (cutout_left, 0),
     (0, 0)
 ]
-population_size = 200
+
+population_size = 20000
+rng = np.random.default_rng()
 low_probability = 10 ** -10
 
 class Localisation:
     def __init__(self):
         self.poses = np.column_stack((
-            np.random.uniform(0, 1500, population_size),
-            np.random.uniform(0, 1500, population_size),
-            np.random.uniform(0, 2 * np.pi, population_size)
+            rng.uniform(0, width, population_size),
+            rng.uniform(0, height, population_size),
+            rng.uniform(0, 2 * np.pi, population_size)
         ))
         self.wheel_distance = 0
 
@@ -37,18 +48,25 @@ class Localisation:
 
     def in_boundary(self):
         inside_walls = np.logical_and(
-            np.logical_and(self.poses[:, 0] > 0, self.poses[:, 0] < 1500),
-            np.logical_and(self.poses[:, 1] > 0, self.poses[:, 1] < 1500)
+            np.logical_and(self.poses[:, 0] > 0, self.poses[:, 0] < width),
+            np.logical_and(self.poses[:, 1] > 0, self.poses[:, 1] < height)
         )
         not_in_cutouts = np.logical_not(
-            np.logical_and(self.poses[:, 0] < 1000, self.poses[:, 1] > 500)
+            np.logical_and(self.poses[:, 0] > cutout_left, self.poses[:, 1] < cutout_top)
         )
         return np.logical_and(inside_walls, not_in_cutouts)
 
     def observational_model(self):
-        weights = np.ones(population_size, dtype=np.float32)
-        weights = np.where(np.logical_not(self.in_boundary()), weights, low_probability)
+        weights = np.where(self.in_boundary(), 1.0, low_probability)
         return weights
+
+    def resample_poses(self, weights, sample_count):
+        normalised_weights = weights / np.sum(weights)
+        return rng.choice(
+            self.poses,
+            size=sample_count,
+            p=normalised_weights
+        )
 
     def on_config_updated(self, client, userdata, message):
         self.config_ready = True
@@ -56,8 +74,8 @@ class Localisation:
         if 'robot/wheel_distance' in data:
             self.wheel_distance = data['robot/wheel_distance']
 
-    def publish_poses(self, client):
-        publish_json(client, "localisation/poses", self.poses.tolist())
+    def publish_poses(self, client, poses):
+        publish_json(client, "localisation/poses", poses.tolist())
 
     def publish_map(self, client):
         publish_json(client, "localisation/map", {
@@ -79,8 +97,8 @@ class Localisation:
             + self.alpha_trans_rot * abs(rotation)
         rot_scale = self.alpha_rot_rot * abs(rotation) \
             + self.alpha_rot_trans * abs(translation)
-        trans_samples = np.random.normal(translation, trans_scale, population_size)
-        rot_samples = np.random.normal(rotation, rot_scale, population_size)
+        trans_samples = rng.normal(translation, trans_scale, population_size)
+        rot_samples = rng.normal(rotation, rot_scale, population_size)
         return trans_samples, rot_samples
 
     def move_poses(self, rotation, translation):
@@ -95,20 +113,19 @@ class Localisation:
         right_distance_delta = distance_data['right_distance'] - self.previous_right_distance
         self.previous_left_distance = distance_data['left_distance']
         self.previous_right_distance = distance_data['right_distance']
-
         # Think
         translation, theta = self.convert_encoders_to_motion(left_distance_delta, right_distance_delta)
         rotation = theta / 2
         trans_samples, rot_samples = self.randomise_motion(translation, rotation)
         self.move_poses(rot_samples, trans_samples)
         weights = self.observational_model()
-        print(weights)
+        self.poses = self.resample_poses(weights, population_size)
         # Act
-        self.publish_poses(client)
+        publish_sample = self.resample_poses(weights, 100)
+        self.publish_poses(client, publish_sample)
 
     def start(self):
         client = connect()
-        self.publish_poses(client)
         self.publish_map(client)
         print("Waiting for config")
         client.subscribe("config/updated")
@@ -119,7 +136,7 @@ class Localisation:
                         ])
         while not self.config_ready:
             time.sleep(0.1)
-        print("Config received. Now wiating for sensor data...")
+        print("Config received. Now waiting for sensor data...")
 
         client.subscribe("sensors/encoders/data")
         client.publish("sensors/encoders/control/reset")
