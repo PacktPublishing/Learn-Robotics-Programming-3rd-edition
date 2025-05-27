08161cba7475a203204d636eed5c008e336434ed
Danny Staple <danny@orionrobots.co.uk>
2025-05-27 20:31:49 +0100

    Tuning as I write

--- robot/look_at_face.py
+++ robot/look_at_face.py
@@ -1,35 +1,37 @@
 import time
 import json
 
+import numpy as np
+
 from common.mqtt_behavior import connect, publish_json
 from common.pid_controller import PIDController
 
+
+
 pan_range = 70
 tilt_range = 50
-class LookAtFaceBehavior:
-    center_x = 320
-    center_y = 240
+center_x = 320
+center_y = 240
 
+class LookAtFaceBehavior:
     def __init__(self):
-        self.face_x = self.center_x
-        self.face_y = self.center_y
+        self.face_x = center_x
+        self.face_y = center_y
 
     def look_at_face(self, client):
-        self.tilt_pid = PIDController(0, 0.01)
-        self.pan_pid = PIDController(0, 0.01)
+        self.pan_pid = PIDController(0, 0.006)
+        self.tilt_pid = PIDController(0, -0.006)  # Inverted tilt
         while True:
             # Think
-            pan_error = self.center_x - self.face_x
-            tilt_error = self.center_y - self.face_y
-
-            pan_position = max(-pan_range, min(pan_range, self.pan_pid.control(pan_error)))
-            tilt_position = max(-tilt_range, min(tilt_range, self.tilt_pid.control(tilt_error)))
-            print("Pan position:", pan_position)
-            print("Tilt position:", tilt_position)
-
+            pan_error = center_x - self.face_x
+            tilt_error = center_y - self.face_y
+            pan_position = self.pan_pid.control(pan_error)
+            tilt_position = self.tilt_pid.control(tilt_error)
+            pan_position = np.clip(pan_position, -pan_range, pan_range)
+            tilt_position = np.clip(tilt_position, -tilt_range, tilt_range)
             # Act
             publish_json(client, "motors/servo/pan/position", pan_position)
-            publish_json(client, "motors/servo/tilt/position", -tilt_position) # Inverted tilt
+            publish_json(client, "motors/servo/tilt/position", tilt_position)
             time.sleep(0.05)
 
     def on_faces_detected(self, client, userdata, msg):
@@ -43,8 +45,8 @@ class LookAtFaceBehavior:
             print("Largest face detected at:", self.face_x, self.face_y)
         else:
             # If no face is detected, reset the face coordinates to the center
-            self.face_x = self.center_x
-            self.face_y = self.center_y
+            self.face_x = center_x
+            self.face_y = center_y
 
     def start(self):
         client = connect()
