7e65e6fa7b90c2395c820b7eb8def5ce80b77a82
Danny Staple <danny@orionrobots.co.uk>
2025-08-09 13:55:15 +0100

    Base packages should have the visual packages here too.

--- deploy/base_packages.py
+++ deploy/base_packages.py
@@ -2,9 +2,7 @@ from pyinfra.operations import apt
 
 base_packages = apt.packages(
     name="Install python and i2c tools",
-    packages=["python3-pip", "python3-smbus", "i2c-tools", "python3-ujson", "python3-numpy",
-              "python3-picamera2", "python3-opencv", "opencv-data",
-              "python3-flask"],
+    packages=["python3-pip", "python3-smbus", "i2c-tools", "python3-ujson", "python3-numpy"],
     update=True,
     no_recommends=True,
     _sudo=True,
