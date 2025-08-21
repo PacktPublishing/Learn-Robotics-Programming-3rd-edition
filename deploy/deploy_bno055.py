0e3278cbd329321926957256d31d282330060be9
Danny Staple <danny@orionrobots.co.uk>
2025-08-21 20:32:09 +0100

    These too

--- deploy/deploy_bno055.py
+++
@@ -1,8 +0,0 @@
-from pyinfra.operations import pip
-from deploy import virtual_env
-
-pip.packages(
-    name="Install BNO055 dependencies",
-    packages=["Adafruit-Blinka", "adafruit-circuitpython-bno055"],
-    virtualenv=virtual_env.robot_venv,
-)
