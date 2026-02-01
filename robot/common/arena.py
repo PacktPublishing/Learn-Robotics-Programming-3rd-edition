f866693c99697d47586dfc09cf6051bac89530d4
Danny Staple <danny@orionrobots.co.uk>
2026-02-01 14:55:36 +0000

    Probability field drawn

--- robot/common/arena.py
+++ robot/common/arena.py
@@ -1,3 +1,4 @@
+import numpy as np
 from common.mqtt_behavior import publish_json
 
 left = 0
@@ -6,16 +7,41 @@ right = 1500
 top = 1500
 cutout_left = 1000
 cutout_top = 500
-walls = [
+walls = np.array([
     (left, top),
     (right, top),
     (right, cutout_top),
     (cutout_left, cutout_top),
     (cutout_left, bottom),
     (left, bottom)
-]
+])
 
 def publish_map(client):
     publish_json(client, "localisation/map", {
         "walls": walls
     })
+
+class MapFrame:
+    def __init__(self, margin: int):
+        """Mapped coordinates represents a margin around the world coordinates.
+        The extents of this should be
+        """
+        self.w_left = -margin
+        self.w_right = right + margin
+        self.w_bottom = -margin
+        self.w_top = top + margin
+        self.margin = margin
+
+    @property
+    def width(self) -> int:
+        return self.w_right - self.w_left
+
+    @property
+    def height(self) -> int:
+        return self.w_top - self.w_bottom
+
+    def world_to_map(self, world_coordinates: np.ndarray) -> np.ndarray:
+        return world_coordinates + np.array([self.margin, self.margin])
+
+    def map_to_world(self, map_coordinates: np.ndarray) -> np.ndarray:
+        return map_coordinates - np.array([self.margin, self.margin])
