82bfc7360795a44d19f1d30b9576c6d484990a3e
Danny Staple <danny@orionrobots.co.uk>
2026-03-22 12:05:07 +0000

    import: chapter-7 complete snapshot from origin/chapter-7/complete

---
+++ chapter-7/complete/robot/common/mqtt_behavior.py
@@ -0,0 +1,25 @@
+import paho.mqtt.client as mqtt
+import time
+import json
+
+def default_on_connect(client, userdata, flags, rc):
+    print(f"Connected with result code {rc}")
+
+def connect(on_connect=default_on_connect, start_loop=True):
+    mqtt_username = "robot"
+    mqtt_password = "robot"
+
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.username_pw_set(mqtt_username, mqtt_password)
+    client.will_set("all/stop", "")
+    client.connect("localhost", 1883)
+
+    if start_loop:
+        client.loop_start()
+        while not client.is_connected():
+            time.sleep(0.01)
+    return client
+
+def publish_json(client, topic, data):
+    client.publish(topic, json.dumps(data))
