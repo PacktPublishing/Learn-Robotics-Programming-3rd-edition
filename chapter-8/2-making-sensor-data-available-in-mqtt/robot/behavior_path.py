0227f1efff66c5511c99df836254cdecf0ce369b
Danny Staple <danny@orionrobots.co.uk>
2026-03-22 12:23:23 +0000

    import: chapter-8 stage 2-making-sensor-data-available-in-mqtt from origin/chapter-8/2-making-sensor-data-available-in-mqtt

---
+++ chapter-8/2-making-sensor-data-available-in-mqtt/robot/behavior_path.py
@@ -0,0 +1,28 @@
+from time import sleep
+from common.mqtt_behavior import connect, publish_json
+
+def forward(client, seconds):
+    publish_json(client, "motors/wheels", [0.8, 0.8])
+    sleep(seconds)
+
+def drive_left(client, seconds):
+    publish_json(client, "motors/wheels", [0.2, 0.8])
+    sleep(seconds)
+
+def drive_right(client, seconds):
+    publish_json(client, "motors/wheels", [0.8, 0.2])
+    sleep(seconds)
+
+def spin_left(client, seconds):
+    publish_json(client, "motors/wheels", [-0.8, 0.8])
+    sleep(seconds)
+
+client = connect()
+forward(client, 1)
+drive_right(client, 1)
+forward(client, 1)
+drive_left(client, 1)
+forward(client, 1)
+drive_left(client, 1)
+forward(client, 1)
+spin_left(client, 1)
