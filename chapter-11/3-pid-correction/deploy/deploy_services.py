11cb6b85674e3a1b08b89a3e54907d0903ff64f1
Danny Staple <danny@orionrobots.co.uk>
2026-03-22 12:22:54 +0000

    import: chapter-11 stage 3-pid-correction from origin/chapter-11/3-pid-correction

---
+++ chapter-11/3-pid-correction/deploy/deploy_services.py
@@ -0,0 +1,139 @@
+from pyinfra.operations import files, systemd
+from pyinfra import host
+import os
+
+
+def deploy_service(service_name, command, auto_start, changed):
+    if auto_start:
+        restart = "always"
+    else:
+        restart = "no"
+
+    unit_file = files.template(
+        name=f"Create {service_name} service",
+        src="deploy/service_template.j2",
+        dest=f"/etc/systemd/system/{service_name}.service",
+        pi_user=host.data.get('ssh_user'),
+        service_name=service_name,
+        command=command,
+        restart=restart,
+        _sudo=True
+    )
+
+    if changed or code.changed or unit_file.changed:
+        systemd.service(
+            name=f"Restart {service_name} service",
+            service=service_name,
+            running=auto_start,
+            enabled=auto_start,
+            restarted=auto_start,
+            daemon_reload=unit_file.changed,
+            _sudo=True,
+        )
+
+
+common = files.sync(
+    name="Update common code",
+    src="robot/common", dest="robot/common")
+
+code = files.put(
+    name="Update inventor hat code",
+    src="robot/inventor_hat_service.py", dest="robot/inventor_hat_service.py")
+
+deploy_service("inventor_hat_service", "robot/inventor_hat_service.py",
+               True, common.changed or code.changed)
+
+code = files.put(
+    name="Update launcher code",
+    src="robot/launcher_service.py",
+    dest="robot/launcher_service.py")
+deploy_service("launcher_service", "robot/launcher_service.py",
+               True, common.changed or code.changed)
+
+code = files.put(
+    name="Update behavior_path code",
+    src="robot/behavior_path.py",
+    dest="robot/behavior_path.py")
+deploy_service("behavior_path", "robot/behavior_path.py",
+               False, common.changed or code.changed)
+
+code = files.put(
+    name="Update drive_known_distance code",
+    src="robot/drive_known_distance.py",
+    dest="robot/drive_known_distance.py")
+deploy_service("drive_known_distance", "robot/drive_known_distance.py",
+               False, common.changed or code.changed)
+
+
+code = files.put(
+    name="Update drive_straight_line code",
+    src="robot/drive_straight_line.py",
+    dest="robot/drive_straight_line.py")
+deploy_service("drive_straight_line", "robot/drive_straight_line.py",
+               False, common.changed or code.changed)
+
+
+code = files.put(
+    name="Update circle_head code",
+    src="robot/circle_head_behavior.py",
+    dest="robot/circle_head_behavior.py")
+deploy_service("circle_head", "robot/circle_head_behavior.py",
+               False, common.changed or code.changed)
+
+code = files.put(
+    name="Update distance sensor service",
+    src="robot/distance_sensor_service.py",
+    dest="robot/distance_sensor_service.py")
+deploy_service("distance_sensor_service",
+               "robot/distance_sensor_service.py",
+               True, common.changed or code.changed)
+
+code = files.put(
+    name="Update fixed distance avoider",
+    src="robot/fixed_distance_avoider.py",
+    dest="robot/fixed_distance_avoider.py")
+deploy_service("fixed_distance_avoider",
+               "robot/fixed_distance_avoider.py",
+               False, common.changed or code.changed)
+
+code = files.put(
+    name="Update smooth distance avoider",
+    src="robot/smooth_distance_avoider.py",
+    dest="robot/smooth_distance_avoider.py")
+deploy_service("smooth_distance_avoider",
+               "robot/smooth_distance_avoider.py",
+               False, common.changed or code.changed)
+
+files.directory(
+    name="Create robot_control/libs",
+    path="robot_control/libs"
+)
+files.download(
+    name="Download joystick widget",
+    src="https://github.com/bobboteck/JoyStick/raw/master/joy.js",
+    dest="robot_control/libs/joy.js"
+)
+files.download(
+    name="Download mqtt js",
+    src="https://unpkg.com/mqtt@5.7.0/dist/mqtt.esm.js",
+    dest="robot_control/libs/mqtt.js"
+)
+
+# Loop over all the files in the robot_control directory
+pages_folder = files.directory(
+    name="Create robot_control folder",
+    path="robot_control"
+)
+pages = [
+    files.template(
+        name=f"Deploy {file_name}",
+        src=f"robot_control/{file_name}",
+        dest=f"robot_control/{file_name}",
+    )
+    for file_name in os.listdir("robot_control")
+]
+
+pages_changed = pages_folder.changed or any(page.changed for page in pages)
+
+deploy_service("web_server", "-m http.server --directory robot_control 80",
+               True, pages_changed)
---
+++ chapter-11/3-pid-correction/robot/drive_straight_line.py
@@ -0,0 +1,38 @@
+import ujson as json
+import time
+
+from common.mqtt_behavior import connect, publish_json
+from common.pid_controller import PIDController
+
+
+class DriveStraightLineBehavior:
+    def __init__(self):
+        self.speed = 0.6
+        self.balance_pid = PIDController(0.01)
+
+    def on_encoders_data(self, client, userdata, message):
+        encoder_data = json.loads(message.payload)
+        left_encoder = encoder_data['left_distance']
+        right_encoder = encoder_data['right_distance']
+
+        # Get the error
+        error = left_encoder - right_encoder
+        publish_json(client, "drive_straight_line/plot", {"error": error, "time": time.time()})
+
+        # Balance the motors
+        balance = self.balance_pid.control(error)
+        left_speed = self.speed - balance
+        right_speed = self.speed + balance
+        publish_json(client, "motors/wheels", [left_speed, right_speed])
+
+    def start(self):
+        client = connect(start_loop=False)
+        client.subscribe("sensors/encoders/data")
+        client.publish("sensors/encoders/control/reset")
+        client.message_callback_add("sensors/encoders/data",
+                                    self.on_encoders_data)
+        client.loop_forever()
+
+
+behavior = DriveStraightLineBehavior()
+behavior.start()
