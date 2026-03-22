f6195ecd0c733383ffec19df87f8c701809380ce
Danny Staple <danny@orionrobots.co.uk>
2026-03-22 12:22:54 +0000

    import: chapter-10 stage original from origin/chapter-10/original

---
+++ chapter-10/original/robot/tests/test_distance_sensor.py
@@ -0,0 +1,16 @@
+import vl53l5cx_ctypes
+import time
+
+sensor = vl53l5cx_ctypes.VL53L5CX()
+sensor.set_resolution(8 * 8)
+sensor.set_ranging_frequency_hz(10)
+sensor.start_ranging()
+
+try:
+    while True:
+        if sensor.data_ready():
+            data = sensor.get_data()
+            print(data.distance_mm[0][0], data.distance_mm[0][7])
+        time.sleep(0.01)
+finally:
+    sensor.stop_ranging()
