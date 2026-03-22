352e6b991ba16bafea37b54bce6879f2d67e9b99
Danny Staple <danny@orionrobots.co.uk>
2026-03-22 12:07:54 +0000

    import: chapter-17 stage mosquitto-password-trixie from origin/chapter-17/mosquitto-password-trixie

---
+++ chapter-17/mosquitto-password-trixie/deploy/deploy_mqtt.py
@@ -0,0 +1,47 @@
+from pyinfra.operations import \
+    apt, systemd, server, files
+
+
+mosquitto_packages = apt.packages(
+    name="Install mosquitto",
+    packages=[
+        "mosquitto",
+        "mosquitto-clients",
+        "python3-paho-mqtt"
+    ],
+    present=True, _sudo=True)
+
+mqtt_username = "robot"
+mqtt_password = "robot"
+if mosquitto_packages.changed:
+    # set mosquitto password
+    server.shell(
+        f"mosquitto_passwd -c -b /etc/mosquitto/passwd {mqtt_username} {mqtt_password}",
+        _sudo=True)
+
+files.file(
+    name="Ensure mosquitto password file exists",
+    path="/etc/mosquitto/passwd",
+    present=True,
+    user="mosquitto",
+    group="mosquitto",
+    _sudo=True
+)
+
+mosquitto_files = files.put(
+    name="Configure mosquitto",
+    src="deploy/robot_mosquitto.conf",
+    dest="/etc/mosquitto/conf.d/robot.conf",
+    _sudo=True
+)
+
+if mosquitto_packages.changed or mosquitto_files.changed:
+    # restart mosquitto
+    systemd.service(
+        name="Restart/enable mosquitto",
+        service="mosquitto",
+        running=True,
+        restarted=True,
+        daemon_reload=True,
+        _sudo=True,
+    )
