2cb5ae4ab3939926048fbc30894d228e4cf12bf4
Danny Staple <danny@orionrobots.co.uk>
2026-03-23 23:04:27 +0000

    These shouldn't be here

--- chapter-15/complete_plus_led/robot/behavior_line.py
+++
@@ -1,8 +0,0 @@
-from time import sleep
-from common.mqtt_behavior import connect, publish_json
-
-client = connect()
-publish_json(client, "motors/wheels", [0.8, 0.8])
-sleep(1)
-publish_json(client, "motors/wheels", [0.8, 0.8])
-sleep(1)
